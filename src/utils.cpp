7a855ad1c80675e875553e050299758672ea6547
nicolacalzone <nicolacalzone14@gmail.com>
2023-12-23 23:37:40 +0100

    Removed

--- src/utils.cpp
+++ src/utils.cpp
@@ -11,23 +11,3 @@ std::pair<float, float> polarToCartesian(float r, float theta){
     return std::make_pair(X, Y);
 }
 
-/**
-     * @brief Convert std::vector<Obstacle> to ir2324_group_10::Obstacle[] (--> message type)
-     * @param obstacles The obstacles to convert.
-     * @return Vector of obstacles in message format.
-     */
-    std::vector<ir2324_group_10::Obstacle> convertToMsgType(const std::vector<Obstacle>& obstacles) {
-        std::vector<ir2324_group_10::Obstacle> msgObstacles;
-
-        for (const Obstacle& obstacle : obstacles) 
-        {
-            ir2324_group_10::Obstacle msgObstacle;
-            msgObstacle.x = obstacle.getX();
-            msgObstacle.y = obstacle.getY();
-            msgObstacle.radius = obstacle.getRadius();
-
-            msgObstacles.push_back(msgObstacle);
-        }
-
-        return msgObstacles;
-    }
--- src/utils.h
+++ src/utils.h
@@ -6,10 +6,8 @@
 #include <cmath>
 #include <utility>
 #include <cmath>
-#include <ir2324_group_10/PoseAction.h>
 
 double degreesToRadians(double degrees);
 std::pair<float, float> polarToCartesian(float r, float theta);
-std::vector<ir2324_group_10::Obstacle> convertToMsgType(const std::vector<Obstacle>& obstacles);
 
 #endif
