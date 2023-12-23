36ae49e9d6c28437d60dc9e13e2664fa1299bbfd
Leonardo Da Re <AD\darleo14808@DEI-INT-ROB-008.vdi.ict.unipd.it>
2023-12-23 22:43:46 +0100

    scan_methods commented

--- src/scan_methods.cpp
+++ src/scan_methods.cpp
@@ -1,31 +1,45 @@
 #include "scan_methods.h"
 #include <math.h>
 
+/**
+ * @brief Computes clusters from the ranges fiels of /scan topic message
+ * @param ranges The vector ranges
+ * @param th1 Threshold for accepting two consecutive distances in the same cluster
+ */
 std::vector<std::vector<float>> clusterRanges(const std::vector<float>& ranges, float th1)
 {	
+	// creation of the structure that contains the clusters
     std::vector<std::vector<float>> clusters;
     std::vector<float> currentCluster;
-    int start_index = 0;
 
     for (int i = 0; i < ranges.size(); i++) {
+    	// first condition is to start populate the cluster
+    	// second condition: add the next measurement to the cluster if the distance is relatively similar (th1)
         if (currentCluster.empty() || (std::abs(ranges[i] - currentCluster.back()) <= th1 ))
         {
             currentCluster.push_back(ranges[i]);
         } else {
+        	// if the difference is too high we move to the next cluster
             clusters.push_back(currentCluster);
             currentCluster = { ranges[i] };
-            start_index = i+1;    
         }
     }
     
     return clusters;
 }
 
-
+/**
+ * @brief Finds obstacles
+ * @param rangeClusters Output of the clusterRanges function
+ * @param angle_min Starting angle for the scan, given by /scan topic
+ * @param angle_increment Increment in the angle, given by /scan topic
+ */
 std::vector<Obstacle> findObstacles1(const std::vector<std::vector<float>>& rangeClusters, float angle_min, float angle_increment)
 {
+	// structure that stores the obstacles
     std::vector<Obstacle> obstacles;
-	
+    
+    // step_sizes measures the difference in distance between adjacent measurements of different clusters
 	std::vector<double> step_sizes;
 	for(int i = 0; i < rangeClusters.size()-1; i++)
 	{
@@ -33,6 +47,7 @@ std::vector<Obstacle> findObstacles1(const std::vector<std::vector<float>>& rang
 		step_sizes.push_back(step);
 	}
 
+	// angle_counter needed for the conversion to cartesian coords
 	int angle_counter = 0;
 	for(int i = 0; i < rangeClusters.size(); i++)
 	{
@@ -41,6 +56,8 @@ std::vector<Obstacle> findObstacles1(const std::vector<std::vector<float>>& rang
 			angle_counter += rangeClusters[i].size();
 		}else
 		{
+		
+			//computation of middle point of the obstacle
 			double x_start = rangeClusters[i][0] * cos(angle_min + angle_increment*angle_counter);
 			double y_start = rangeClusters[i][0] * sin(angle_min + angle_increment*angle_counter);
 			
@@ -52,6 +69,7 @@ std::vector<Obstacle> findObstacles1(const std::vector<std::vector<float>>& rang
 			double x = (x_start + x_end)/2;
 			double y = (y_start + y_end)/2;
 
+			// adding the obstacle to the structure
 			Obstacle o(x, y, i);
 			obstacles.push_back(o);
 			
@@ -61,6 +79,12 @@ std::vector<Obstacle> findObstacles1(const std::vector<std::vector<float>>& rang
     return obstacles;
 }
 
+/**
+ * @brief Finds obstacles
+ * @param rangeClusters Output of the clusterRanges function
+ * @param angle_min Starting angle for the scan, given by /scan topic
+ * @param angle_increment Increment in the angle, given by /scan topic
+ */
 std::vector<Obstacle> findObstacles2(const std::vector<std::vector<float>> &rangeClusters, float angle_min, float angle_increment)
 {
  
@@ -69,7 +93,8 @@ std::vector<Obstacle> findObstacles2(const std::vector<std::vector<float>> &rang
 
     for (int i = 0; i < rangeClusters.size(); i++)
     {
- 
+
+		// computation of the angular coeffs (stored in coeffiecients vector) 
         std::vector<float> currVec = rangeClusters[i];
         std::vector<float> coefficients;
  
@@ -83,11 +108,14 @@ std::vector<Obstacle> findObstacles2(const std::vector<std::vector<float>> &rang
             double x2 = currVec[j + 1] * cos(angle_min + angle_increment * (angle_counter + (j + 1)));
             double x1 = currVec[j] * cos(angle_min + angle_increment * (angle_counter + j));
  
-            m = (y2 - y1) / (x2 - x1);
+            if(x1 == x2) m = 1000000;
+ 			else m = (y2 - y1) / (x2 - x1);
  
             coefficients.push_back(m);
         }
- 
+
+		// we count the number of times difference of adjiacent angular coeffs is >0.1 (because we are taking into consideration the loss of precision)
+		//  if that happens too many times, it means we are not looking at a wall (which is mostly linear)
         int count = 0;
         for (int k = 0; k < coefficients.size() - 1; k++)
         {
@@ -113,6 +141,12 @@ std::vector<Obstacle> findObstacles2(const std::vector<std::vector<float>> &rang
     return obstacles;
 }
 
+/**
+ * @brief Finds obstacles
+ * @param rangeClusters Output of the clusterRanges function
+ * @param angle_min Starting angle for the scan, given by /scan topic
+ * @param angle_increment Increment in the angle, given by /scan topic
+ */
 std::vector<Obstacle> findObstacles3(const std::vector<std::vector<float>> &rangeClusters, float angle_min, float angle_increment)
 {
  
@@ -121,10 +155,13 @@ std::vector<Obstacle> findObstacles3(const std::vector<std::vector<float>> &rang
  
     for (int i = 0; i < rangeClusters.size(); i++)
     {
- 
+ 	
+ 		// computation of the angular coeffs (stored in coeffiecients vector) 
         std::vector<float> currVec = rangeClusters[i];
         std::vector<float> coefficients;
-        if(currVec.size() < 15 || currVec.size() > 70) continue; // condizione per prevenire "spiragli"
+        
+        // condition to avoid the detection of small gaps between objects as objects themselves
+        if(currVec.size() < 15 || currVec.size() > 70) continue; 
  
         for (int j = 0; j < currVec.size() - 1; j++)
         {
@@ -142,10 +179,12 @@ std::vector<Obstacle> findObstacles3(const std::vector<std::vector<float>> &rang
             coefficients.push_back(m);
         }
         
+        // contains the sum of all angular coeffs of a cluster
         double coeff_sum = 0;
         for(int i = 0; i < coefficients.size(); i++) coeff_sum += coefficients[i];
  
-        if (abs(coeff_sum) < 20)
+ 		// coeff_sum should be ~0 if we are detecting a semi-circle, so we set a low threshold
+        if (abs(coeff_sum) < 5)
         {
  
             int indexHalfVector = static_cast<int>(currVec.size() / 2);
